730d4faf05cd4268f828d8891025e755ab07d673
harlinmartin <harlinmartin58@gmail.com>
2026-05-11 14:25:32 +0530

    Authentication Logic and Middleware

--- ecommerce-backend/index.js
+++ ecommerce-backend/index.js
@@ -3,6 +3,9 @@ const dotenv = require('dotenv');
 const cors = require('cors');
 const connectDB = require('./config/dbConnection');
 
+const authRoutes = require('./routes/authenticationRoutes');
+const errorHandler = require('./middleware/errorHandler');
+
 dotenv.config();
 
 const app = express();
@@ -14,10 +17,15 @@ connectDB();
 app.use(cors());
 app.use(express.json());
 
-// Routes Placeholder
+// Routes
+app.use('/api/auth', authRoutes);
+
 app.get('/', (req, res) => {
     res.send('E-Commerce API is running...');
 });
+
+// Error Handler
+app.use(errorHandler);
 
 const PORT = process.env.PORT || 5000;
 
